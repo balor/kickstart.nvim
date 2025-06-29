3e2418803c4da54b5fc3da182c79c54e107bb629
balor <me@balor.pl>
2025-06-29 14:31:48 +0200

    change surround shortcuts so that it wont collide with flash

--- init.lua
+++ init.lua
@@ -850,7 +850,17 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup({
+        mappings = {
+          add = 'gsa',         -- Add surrounding
+          delete = 'gsd',      -- Delete surrounding
+          find = '',           -- Optional: disable if unused
+          find_left = '',      -- Optional: disable if unused
+          highlight = '',      -- Optional: disable if unused
+          replace = 'gsr',     -- Replace surrounding
+          update_n_lines = '', -- Optional: disable if unused
+        }
+      })
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
