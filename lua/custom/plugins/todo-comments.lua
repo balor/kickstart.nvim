6c92e34fb6fa3e4b3227bff468f94f143207060f
balor <me@balor.pl>
2025-07-01 15:10:21 +0200

    zen mode keys

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -1,9 +1,6 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-
-  },
   keys = {
     { '<leader>l', ':TodoTelescope keywords=TODO,FIX,LATER,WARN<CR>', desc = 'Todo [L]ist QuickFix' },
   },
--- lua/custom/plugins/zen-mode.lua
+++ lua/custom/plugins/zen-mode.lua
@@ -1,5 +1,9 @@
 return {
   "folke/zen-mode.nvim",
+  keys = {
+    { '<leader>z', ':TodoTelescope keywords=TODO,FIX,LATER,WARN<CR>', desc = 'Todo [L]ist QuickFix' },
+  },
+
   config = function()
     zm = require('zen-mode');
 
